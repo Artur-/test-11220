1d112273a127d9d663f09776bfeb0d8e8c97c2b6
Artur Signell <artur@vaadin.com>
2021-06-15 22:03:29 +0300

    Try to reproduce

--- src/main/java/com/example/application/views/helloworld/HelloWorldView.java
+++ src/main/java/com/example/application/views/helloworld/HelloWorldView.java
@@ -1,32 +1,26 @@
 package com.example.application.views.helloworld;
 
-import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.notification.Notification;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
-import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.textfield.TextField;
-import com.vaadin.flow.router.Route;
-import com.vaadin.flow.router.PageTitle;
 import com.example.application.views.main.MainView;
+import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouteAlias;
+
+import org.vaadin.tarek.CollapsibleSplitLayout;
 
 @Route(value = "hello", layout = MainView.class)
 @RouteAlias(value = "", layout = MainView.class)
 @PageTitle("Hello World")
 public class HelloWorldView extends HorizontalLayout {
 
-    private TextField name;
-    private Button sayHello;
-
     public HelloWorldView() {
-        addClassName("hello-world-view");
-        name = new TextField("Your name");
-        sayHello = new Button("Say hello");
-        add(name, sayHello);
-        setVerticalComponentAlignment(Alignment.END, name, sayHello);
-        sayHello.addClickListener(e -> {
-            Notification.show("Hello " + name.getValue());
-        });
+        CollapsibleSplitLayout cs = new CollapsibleSplitLayout();
+        cs.setSizeFull();
+        cs.addToPrimary(new Span("Primary"));
+        cs.addToSecondary(new Span("Secondary"));
+        add(cs);
+
     }
 
 }
